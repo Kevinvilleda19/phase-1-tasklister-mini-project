52b470afdcd143b71a866d49b3b7cf95667c7224
Kevin <kvilleda10566@gmail.com>
2024-08-09 16:39:16 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (event) => {
 });
+const form = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#list');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+const taskDescription = document.querySelector('#new-task-description').value;
+
+const taskListItem = document.createElement('li');
+taskListItem.textContent = taskDescription;
+
+
+taskList.appendChild(taskListItem);
+
+
+document.querySelector('#new-task-description').value = '';
+});
+
